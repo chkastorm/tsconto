d73ab67e109ab4aab1e7865ea31d8065f31963c7
Kastor M <51870506+chkastorm@users.noreply.github.com>
2022-01-17 12:06:27 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -14,7 +14,7 @@
 mv /tmp/tsconto-1.0.0/tsconto.py /usr/local/bin/tsconto
 chmod 755 /usr/local/bin/tsconto
 chown root:root /usr/local/bin/tsconto
-rm -f /tmp/tsconto-1.0.0.tar.gz
+rm -f /tmp/v1.0.0.tar.gz
 rm -rf /tmp/tsconto-1.0.0
 echo ""
 echo "Install completed. Please issue \"tsconto\" to find the user manual."
