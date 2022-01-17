93985b9e756f0a89475de67cf71620a45649da75
Kastor M <51870506+chkastorm@users.noreply.github.com>
2022-01-17 14:45:59 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,16 +6,16 @@
 ### Abbreviation : tsconto                                                   ###
 ### Author : Kastor M.                                                       ###
 ### Modified : Kastor M.                                                     ###
-### Version : 1.0.0                                                          ###
-### Date : Mon Jan 17 03:43:42 UTC 2022                                      ###
+### Version : 1.0.1                                                          ###
+### Date : Mon Jan 17 19:45:44 UTC 2022                                      ###
 ###                                                                          ###
 ################################################################################
 
-mv /tmp/tsconto-1.0.0/tsconto.py /usr/local/bin/tsconto
+mv /tmp/tsconto-1.0.1/tsconto.py /usr/local/bin/tsconto
 chmod 755 /usr/local/bin/tsconto
 chown root:root /usr/local/bin/tsconto
-rm -f /tmp/v1.0.0.tar.gz
-rm -rf /tmp/tsconto-1.0.0
+rm -f /tmp/v1.0.1.tar.gz
+rm -rf /tmp/tsconto-1.0.1
 echo ""
 echo "Install completed. Please issue \"tsconto\" to find the user manual."
 echo ""
