87961fa439d713101af34b89215f9cbd72672915
Kastor M <51870506+chkastorm@users.noreply.github.com>
2022-04-10 15:23:18 -0400

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -6,15 +6,15 @@
 ### Abbreviation : tsconto                                                   ###
 ### Author : Kastor M.                                                       ###
 ### Modified : Kastor M.                                                     ###
-### Version : 1.0.2                                                          ###
-### Date : Tue 29 Mar 2022 10:45:28 PM UTC                                   ###
+### Version : 1.0.3                                                          ###
+### Date : Sun 10 Apr 2022 03:20:16 PM EDT                                   ###
 ###                                                                          ###
 ################################################################################
 
 rm -f /usr/local/bin/tsconto
 rm -rf ~/.rhpn-gns3-stsc/
-rm -f /tmp/v1.0.2.tar.gz
-rm -rf /tmp/tsconto-1.0.2
+rm -f /tmp/v1.0.3.tar.gz
+rm -rf /tmp/tsconto-1.0.3
 echo ""
 echo "All components from tsconto are removed. Thank you for your support."
 echo ""
